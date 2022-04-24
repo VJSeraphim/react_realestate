1f568d6266940745f9d8a32cd9f6e79289831c4d
VJS <minsung_96@naver.com>
2022-04-24 23:58:16 +0900

    Api Init

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Flex, Box, Text, Button } from '@chakra-ui/react'
 
+import { baseUrl, fetchApi } from '../utils/fetchApi'
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexwrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner"/>
@@ -19,7 +21,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
         <br />
         {desc2}
       </Text>
-      <Button fontSize="xl" bg="blue.300" color="white">
+      <Button fontSize="xl">
         <Link href={linkName}>
           {buttonText}
         </Link>
@@ -30,7 +32,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 
 export default function Home() {
   return (
-    <div>
+    <box>
       <Banner 
         purpose="RENT HOME"
         title1="Rental Home for"
@@ -39,11 +41,29 @@ export default function Home() {
         desc2="and more"
         buttonText="Explore Renting"
         LinkName="/search?purpose=for-rent"
-        imageUrl=""
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">
+        {/* fetching data from API Code Here...*/}
+      </Flex>
       <Banner 
-        purpose={'For Rent'}
+        purpose='BUY A HOME'
+        title1='Find, Buy & Own Your '
+        title2='Dream Home'
+        desc1='Explore from Apartments, land, builder floors, '
+        desc2='Villas and more'
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
-    </div>
+      <Flex flexWrap="wrap">
+        {/* fetching data from API Code Here...*/}
+      </Flex>
+    </box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`)
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`)
 }
