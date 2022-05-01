7c97bd9fd03c81c2b34d90f028dc4aa29bf9e26a
VJS <minsung_96@naver.com>
2022-05-01 23:57:51 +0900

    Search #2

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import { Flex, Box, Text, Select, Input, Spinner, Icon, Button} from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next-image'
+
+const SearchFilters = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchFilters
